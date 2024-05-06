c31b90d225f298a72d6de8fbb71c7656da493d49
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-05-06 21:50:01 +0800

    Move the types.

--- src/services/octal/get_octal_to_binary.go
+++ src/services/octal/get_octal_to_binary.go
@@ -1,16 +1,5 @@
 package octal
 
-var octalSystemMap = map[int]string{
-	0: "000",
-	1: "001",
-	2: "010",
-	3: "011",
-	4: "100",
-	5: "101",
-	6: "110",
-	7: "111",
-}
-
 func getOctalToBinary(octal int64) string {
 	octalArray := getNumberArray(octal)
 	binary := ""
