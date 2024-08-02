4a3148bcc765202c2f721037e180f9fbdf3b7f45
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-02 14:30:08 +0800

    Add the app_options file.

--- src/main.c
+++ src/main.c
@@ -1,4 +1,5 @@
 #include "displays/app_description.h"
+#include "displays/app_options.h"
 #include "displays/clear_terminal.h"
 #include "displays/escape_sequence.h"
 #include <stdio.h>
@@ -12,6 +13,11 @@ int main(void) {
   // App description.
   app_description();
   add_new_tab();
+  add_new_line();
+
+  // Show the app options.
+  app_options();
+  add_new_line();
   add_new_line();
 
   add_new_line();
