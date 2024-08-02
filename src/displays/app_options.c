a3f0b5bb64cd8b5a556f06b30b7fea0db1d2a0a5
Kent Louise Tonino <tonino.kentlouise@gmail.com>
2024-08-02 14:54:45 +0800

    Formate the options.

--- src/displays/app_options.c
+++ src/displays/app_options.c
@@ -1,19 +1,26 @@
 #include "ansi_color.h"
 #include "escape_sequence.h"
+#include <stdlib.h>
+
+char *option(int num) {
+  char *formatted_option = malloc(sizeof(char) * 50);
+
+  // Write the formatted output in formatted_option variable.
+  sprintf(formatted_option, "%-16s[%s %s%d%s %s]%s", BLUE, RESET, GREEN, num,
+          RESET, BLUE, RESET);
+
+  return formatted_option;
+}
 
 void app_options(void) {
-  printf("%-16s[%s %s1%s %s]%s Convert Binary Numbers", BLUE, RESET, GREEN,
-         RESET, BLUE, RESET);
+  printf("%s Convert Binary", option(1));
   add_new_line();
-  printf("%-16s[%s %s2%s %s]%s Convert Decimal Numbers", BLUE, RESET, GREEN,
-         RESET, BLUE, RESET);
+  printf("%s Convert Decimal", option(2));
   add_new_line();
-  printf("%-16s[%s %s3%s %s]%s Convert Octal Numbers", BLUE, RESET, GREEN,
-         RESET, BLUE, RESET);
+  printf("%s Convert Octal", option(3));
   add_new_line();
-  printf("%-16s[%s %s4%s %s]%s Convert Hexadecimal Numbers", BLUE, RESET, GREEN,
-         RESET, BLUE, RESET);
+  printf("%s Convert Hexadecimal", option(4));
   add_new_line();
-  printf("%-16s[%s %s5%s %s]%s Exit", BLUE, RESET, GREEN, RESET, BLUE, RESET);
+  printf("%s Exit", option(5));
   add_new_line();
 }
