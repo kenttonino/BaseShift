a31d21f0accf79ebaac8780e86bec92556fd0567
Kent Tonino <tonino.kentlouise@gmail.com>
2025-10-05 04:33:10 +0800

    feat: apply blue custom print
    Signed-off-by: Kent Tonino <tonino.kentlouise@gmail.com>

--- src/number_systems/binary_system/binary_description.c
+++ src/number_systems/binary_system/binary_description.c
@@ -3,11 +3,9 @@
 #include "../../displays/escape_sequence.h"
 
 void binary_description(void) {
-  printf("%-15s--------------------------------------------------------%s",
-         BLUE, RESET);
+  text_blue("--------------------------------------------------------", 15);
   add_new_line(2);
   text_green("Binary to Other System", 32);
   add_new_line(2);
-  printf("%-15s--------------------------------------------------------%s",
-         BLUE, RESET);
+  text_blue("--------------------------------------------------------", 15);
 }
