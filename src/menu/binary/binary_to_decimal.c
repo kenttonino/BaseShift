6d5406ace2a457799d8973811c97bea7d211f4b6
Kent Tonino <tonino.kentlouise@gmail.com>
2026-02-14 00:30:26 +0800

    feat: separate binary values before and after dot

--- src/menu/binary/binary_to_decimal.c
+++ src/menu/binary/binary_to_decimal.c
@@ -23,6 +23,15 @@ int is_negative_only(char* binary_input) {
   }
 }
 
+// If the binary input does include a negative and dot.
+int is_negative_with_dot(char* binary_input) {
+  if (strrchr(binary_input, '-') != NULL && strrchr(binary_input, '.') != NULL) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 void binary_to_decimal(char *binary_input) {
   // E.g. 0001 value.
   if (is_normal(binary_input)) {
@@ -55,7 +64,7 @@ void binary_to_decimal(char *binary_input) {
 
   // E.g. -0001 value.
   if (is_negative_only(binary_input)) {
-    // Remove the first character.
+    // Remove the - character.
     memmove(binary_input, binary_input+1, strlen(binary_input));
 
     // Handle the summation of the binary values.
@@ -82,6 +91,41 @@ void binary_to_decimal(char *binary_input) {
         RESET
     );
 
+    return;
+  }
+
+  // E.g. -0001.1 value.
+  if (is_negative_with_dot(binary_input)) {
+    // Remove the - character.
+    memmove(binary_input, binary_input+1, strlen(binary_input));
+
+    // Separate the binary values before dot.
+    char *binary_before_dot = malloc(strlen(binary_input) + 1);
+    char *binary_after_dot = malloc(strlen(binary_input) + 1);
+    int is_after_dot = 0;
+    int counter_before_dot = 0;
+    int counter_after_dot = 0;
+    for (int i = 0; binary_input[i] != '\0'; i++) {
+      if (binary_input[i] == '.') {
+        is_after_dot = 1;
+        continue;
+      }
+
+      if (is_after_dot) {
+        binary_after_dot[counter_after_dot++] = binary_input[i];
+        continue;
+      } else {
+        binary_before_dot[counter_before_dot++] = binary_input[i];
+      }
+    }
+
+    // Separate the binary values after dot.
+
+    printf("binary_before_dot: %s", binary_before_dot);
+    add_new_line(1);
+    printf("binary_after_dot: %s", binary_after_dot);
+    free(binary_before_dot);
+    free(binary_after_dot);
     return;
   }
 
