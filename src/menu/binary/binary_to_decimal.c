3488794760c1b06febb4e9cb488418b276ac0260
Kent Tonino <tonino.kentlouise@gmail.com>
2026-02-13 22:57:25 +0800

    feat: check if normal binary input

--- src/menu/binary/binary_to_decimal.c
+++ src/menu/binary/binary_to_decimal.c
@@ -5,7 +5,18 @@
 #include <stdlib.h>
 #include <string.h>
 
+// If the binary input does not include a dot (.) or negative (-).
+// 1 means true, 0 means false.
+int is_normal(char* binary_input) {
+  if (strrchr(binary_input, '-') == NULL && strrchr(binary_input, '.') == NULL) {
+    return 1;
+  } else {
+    return 0;
+  }
+}
+
 void binary_to_decimal(char *binary_input) {
+  if (is_normal(binary_input)) {
   char *reversed_binary_input = binary_reverse_string(binary_input);
   int reversed_binary_length = strlen(reversed_binary_input);
   int decimal = 0;
@@ -17,6 +28,9 @@ void binary_to_decimal(char *binary_input) {
     decimal = decimal + (current_binary_int * pow(2, i));
   }
 
-  printf("%-16s[%s %sDecimal%s %s]%s : %s%d%s", BLUE, RESET, GREEN, RESET, BLUE,
+    printf("%-16s[%s %sDecimal%s %s]%s : %s%d%s", BLUE, RESET, GREEN, RESET, BLUE,
          RESET, YELLOW, decimal, RESET);
+
+  }
+
 }
