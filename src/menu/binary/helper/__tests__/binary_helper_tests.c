8eb1ac86d3f44cf10bb3133192cc555e81f719f4
Kent Tonino <kenttonino.io@gmail.com>
2026-05-16 13:39:35 +0800

    feat: unit test for is_positive_binary

--- src/menu/binary/helper/__tests__/binary_helper_tests.c
+++ src/menu/binary/helper/__tests__/binary_helper_tests.c
@@ -1,8 +1,12 @@
 #include "../../../../utils/utils.h"
+#include "test_is_positive_binary.c"
 
 int main(void) {
   clear_terminal();
   add_new_line(2);
 
+  test_is_positive_binary();
+
+  add_new_line(2);
   return 0;
 }
