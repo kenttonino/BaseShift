d51c82bc97d9d2d47a556878d45d3b8c46f39717
Kent Tonino <kenttonino.io@gmail.com>
2026-05-16 13:46:09 +0800

    feat: unit tests for is_positive_binary_with_dot function

--- src/menu/binary/helper/__tests__/binary_helper_tests.c
+++ src/menu/binary/helper/__tests__/binary_helper_tests.c
@@ -1,11 +1,14 @@
 #include "../../../../utils/utils.h"
 #include "test_is_positive_binary.c"
+#include "test_is_positive_binary_with_dot.c"
 
 int main(void) {
   clear_terminal();
   add_new_line(2);
 
   test_is_positive_binary();
+  add_new_line(2);
+  test_is_positive_binary_with_dot();
 
   add_new_line(2);
   return 0;
