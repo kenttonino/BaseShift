3db47841a66f8546074a852a0c540ca9d36a7629
Kent Tonino <kenttonino.io@gmail.com>
2026-05-23 11:28:42 +0800

    feat: get_decimal_with_dot test

--- src/menu/binary/__tests__/binary_tests.c
+++ src/menu/binary/__tests__/binary_tests.c
@@ -1,15 +1,19 @@
 #include "./test_binary_helper.c"
 #include "./test_binary_validate.c"
+#include "./test_binary_to_decimal.c"
 
 int main(void) {
   clear_terminal();
   add_new_line(2);
 
-  // Tests functions;
   test_binary_helper();
   add_new_line(2);
-  test_binary_validate();
 
+  test_binary_to_decimal();
   add_new_line(2);
+
+  test_binary_validate();
+  add_new_line(2);
+
   return 0;
 }
