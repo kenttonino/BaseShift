2c2ac09cf5df001a97deb7d7da6d003fdffbb39a
Kent Tonino <kenttonino.io@gmail.com>
2026-05-24 20:55:42 +0800

    fix: binary_to_octal memory leaks

--- src/menu/binary/binary_to_octal.c
+++ src/menu/binary/binary_to_octal.c
@@ -53,7 +53,7 @@ char *get_octal(char *binary_input) {
   // e.g. "001"
   char *binary_group = malloc(sizeof(char) * 3);
   // e.g. ["001", "000"]
-  char *octal_arrays = malloc(1000);
+  static char *octal_arrays;
 
   for (int i = 0; i <= new_binary_input_len; i++) {
     int binary_group_len = strlen(binary_group);
@@ -78,11 +78,14 @@ char *get_octal(char *binary_input) {
       int octal_arrays_len = strlen(octal_arrays);
 
       octal_arrays[octal_arrays_len] = *octal_value;
+      free(binary_group);
       binary_group = malloc(1000);
       binary_group[0] = new_binary_input[i];
     }
   }
 
+  free(new_binary_input);
+  free(binary_group);
   return octal_arrays;
 }
 
