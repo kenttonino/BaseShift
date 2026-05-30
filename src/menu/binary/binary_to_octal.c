240308294b5b5ddf1ad56b52246f9ef323ce7b8c
Kent Tonino <kenttonino.io@gmail.com>
2026-05-30 13:59:28 +0800

    feat: binary to octal hell yeah

--- src/menu/binary/binary_to_octal.c
+++ src/menu/binary/binary_to_octal.c
@@ -22,16 +22,47 @@ char* zero_adder(char *binary) {
   return binary;
 }
 
+char *octal_mapper(char *binary_group) {
+  if (strcmp(binary_group, "001") == 0) return "1";
+  if (strcmp(binary_group, "010") == 0) return "2";
+  if (strcmp(binary_group, "011") == 0) return "3";
+  if (strcmp(binary_group, "100") == 0) return "4";
+  if (strcmp(binary_group, "101") == 0) return "5";
+  if (strcmp(binary_group, "110") == 0) return "6";
+  if (strcmp(binary_group, "111") == 0) return "7";
+  return "0";
+}
+
 char *get_octal(char *binary_input) {
   char *p_new_binary = zero_adder(binary_input);
-  p_new_binary[strlen(p_new_binary)] = '\0';
+  static char new_binary[1000];
+  strcat(new_binary, p_new_binary);
 
-  for (size_t i = 0; i < strlen(p_new_binary); i++) {
-      printf("%c", p_new_binary[i]);
-      add_new_line(1);
+  char binary_group[3] = "";
+  static char current_binary[1];
+  static char octal[1000] = "";
+
+  for (size_t i = 0; i <= strlen(new_binary); i++) {
+    *current_binary = new_binary[i];
+
+    if (strlen(binary_group) < 3) {
+      strcat(binary_group, current_binary);
+      memset(current_binary, 0, sizeof(char));
+      continue;
+    } else {
+      // Push the octal value of first 3 binary digits.
+      strcat(octal, octal_mapper(binary_group));
+
+      // Reset binary group and assign value for index 0;
+      memset(binary_group, 0, sizeof(char) *3);
+      strcat(binary_group, current_binary);
+
+      // Reset the current_binary for next iteration.
+      memset(current_binary, 0, sizeof(char));
+    }
   }
 
-  return "10";
+  return octal;
 }
 
 void display_octal(char *octal, int negative) {
@@ -56,7 +87,7 @@ void binary_to_octal(char *binary_input) {
   // e.g. 1000 = 10
   if (is_positive_binary(binary_input)) {
     char *p_octal = get_octal(binary_input);
-    display_octal(p_octal, 1);
+    display_octal(p_octal, 0);
     return;
   }
 
