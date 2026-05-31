715fca8eee4f23aff14045b2e6e0c5e34c72f517
Kent Tonino <kenttonino.io@gmail.com>
2026-05-31 08:20:42 +0800

    fix: binary_to_octal bug

--- src/menu/binary/binary_reverse_string.c
+++ src/menu/binary/binary_reverse_string.c
@@ -5,6 +5,7 @@ char *binary_reverse_string(char *input) {
   int current_index = input_length - 1;
   int index_increment = 0;
   static char reversed_string[1000];
+  memset(reversed_string, 0, sizeof(char) * 1000);
 
   for (; current_index > -1; current_index--) {
     reversed_string[index_increment] = input[current_index];
