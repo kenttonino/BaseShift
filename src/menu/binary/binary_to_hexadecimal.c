7b2503b4daccabfab0914e3cd082b0f870b1c5ea
Kent Tonino <kenttonino.io@gmail.com>
2026-06-13 19:20:46 +0800

    feat: hexadecimal zero adder

--- src/menu/binary/binary_to_hexadecimal.c
+++ src/menu/binary/binary_to_hexadecimal.c
@@ -1,9 +1,53 @@
 #include <stdio.h>
+#include <string.h>
 #include "../../utils/utils.h"
 #include "helper/helper.h"
 
-// Output the binary to decimal result.
-void display_hexadecimal(char *decimal, int negative) {
+char *_zero_adder(char* binary_input) {
+  int binary_len = strlen(binary_input);
+  int binary_rem = binary_len % 4;
+
+  if (binary_rem == 1) {
+    char binary_buffer[1000];
+    memset(binary_buffer, 0, sizeof(char) * 1000);
+    memmove(binary_buffer + 3, binary_input, strlen(binary_input) + 3);
+    binary_buffer[0] = '0';
+    binary_buffer[1] = '0';
+    binary_buffer[2] = '0';
+    strcpy(binary_input, binary_buffer);
+    return binary_input;
+  }
+
+  if (binary_rem == 2) {
+    char binary_buffer[1000];
+    memset(binary_buffer, 0, sizeof(char) * 1000);
+    memmove(binary_buffer + 2, binary_input, strlen(binary_input) + 2);
+    binary_buffer[0] = '0';
+    binary_buffer[1] = '0';
+    strcpy(binary_input, binary_buffer);
+    return binary_input;
+  }
+
+  if (binary_rem == 3) {
+    char binary_buffer[1000];
+    memset(binary_buffer, 0, sizeof(char) * 1000);
+    memmove(binary_buffer + 1, binary_input, strlen(binary_input) + 1);
+    binary_buffer[0] = '0';
+    strcpy(binary_input, binary_buffer);
+    return binary_input;
+  }
+
+  return binary_input;
+}
+
+char *_get_hexadecimal(char *binary_input) {
+  char *binary = _zero_adder(binary_input);
+  printf("binary: %s", binary);
+  add_new_line(1);
+  return "8";
+}
+
+void _display_hexadecimal(char *decimal, int negative) {
   char neg_decimal[1000] = "-";
   strcat(neg_decimal, decimal);
 
@@ -22,10 +66,15 @@ void display_hexadecimal(char *decimal, int negative) {
 }
 
 void binary_to_hexadecimal(char *binary_input) {
+  // e.g. 1000 = 8
   if (is_positive_binary(binary_input)) {
-    printf("binary_input: %s", binary_input);
-    add_new_line(1);
-    display_hexadecimal("8", 0);
+    char *binary = malloc(sizeof(char) * 1000);
+    strcpy(binary, binary_input);
+
+    char *p_octal = _get_hexadecimal(binary);
+    _display_hexadecimal(p_octal, 0);
+
+    free(binary);
     return;
   }
 }
