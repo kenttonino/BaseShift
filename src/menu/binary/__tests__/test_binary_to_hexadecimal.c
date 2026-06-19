8bdecf5287e5237bb83b749927534a01c3067926
Kent Tonino <kenttonino.io@gmail.com>
2026-06-19 22:53:02 +0800

    feat: add unit tests for _hex_zero_adder_with_dot

--- src/menu/binary/__tests__/test_binary_to_hexadecimal.c
+++ src/menu/binary/__tests__/test_binary_to_hexadecimal.c
@@ -7,22 +7,41 @@ void test_binary_to_hexadecimal(void) {
   add_new_line(1);
 
   char binary_input[5];
-  memmove(binary_input, "1", 2);
+  memmove(binary_input, "1", sizeof(char) * 2);
   assert(strcmp(_hex_zero_adder(binary_input), "0001") == 0);
-  text_green("_hex_zero_adder -> should return 0001 for 1 value", 10);
+  text_green("_hex_zero_adder -> should return 0001 for 1 digit", 10);
   add_new_line(1);
   memset(binary_input, 0, sizeof(char) * 5);
-  memmove(binary_input, "01", 3);
+  memmove(binary_input, "01", sizeof(char) * 3);
   assert(strcmp(_hex_zero_adder(binary_input), "0001") == 0);
-  text_green("_hex_zero_adder -> should return 0001 for 01 value", 10);
+  text_green("_hex_zero_adder -> should return 0001 for 01 digits", 10);
   add_new_line(1);
   memset(binary_input, 0, sizeof(char) * 5);
-  memmove(binary_input, "001", 4);
+  memmove(binary_input, "001", sizeof(char) * 4);
   assert(strcmp(_hex_zero_adder(binary_input), "0001") == 0);
-  text_green("_hex_zero_adder -> should return 0001 for 001 value", 10);
+  text_green("_hex_zero_adder -> should return 0001 for 001 digits", 10);
   add_new_line(1);
   memset(binary_input, 0, sizeof(char) * 5);
   memmove(binary_input, "0001", sizeof(char) * 5);
   assert(strcmp(_hex_zero_adder(binary_input), "0001") == 0);
-  text_green("_hex_zero_adder -> should return 0001 for 0001 value", 10);
+  text_green("_hex_zero_adder -> should return 0001 for 0001 digits", 10);
+  add_new_line(1);
+
+  memset(binary_input, 0, sizeof(char) * 5);
+  memmove(binary_input, "1", sizeof(char) * 2);
+  assert(strcmp(_hex_zero_adder_with_dot(binary_input), "1000") == 0);
+  text_green("_hex_zero_adder_with_dot -> should return 1000 for 1 digit", 10);
+  add_new_line(1);
+  memset(binary_input, 0, sizeof(char) * 5);
+  memmove(binary_input, "10", sizeof(char) * 3);
+  assert(strcmp(_hex_zero_adder_with_dot(binary_input), "1000") == 0);
+  text_green("_hex_zero_adder_with_dot -> should return 1000 for 10 digits", 10);
+  add_new_line(1);
+  memset(binary_input, 0, sizeof(char) * 5);
+  memmove(binary_input, "100", sizeof(char) * 4);
+  text_green("_hex_zero_adder_with_dot -> should return 1000 for 100 digits", 10);
+  add_new_line(1);
+  memset(binary_input, 0, sizeof(char) * 5);
+  memmove(binary_input, "1000", sizeof(char) * 5);
+  text_green("_hex_zero_adder_with_dot -> should return 1000 for 1000 digits", 10);
 }
