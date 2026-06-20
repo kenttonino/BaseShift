e531168e374ea3c4e5ca25562a3002d6ebf89c6f
Kent Tonino <kenttonino.io@gmail.com>
2026-06-20 13:26:48 +0800

    chore: change test names

--- src/menu/binary/__tests__/test_binary_to_octal.c
+++ src/menu/binary/__tests__/test_binary_to_octal.c
@@ -24,26 +24,26 @@ void test_binary_to_octal(void) {
   add_new_line(1);
 
   assert(strcmp(_octal_mapper("000"), "0") == 0);
-  text_green("octal_mapper -> should return 0 for 000 value", 10);
+  text_green("_octal_mapper -> should return 0 for 000 value", 10);
   add_new_line(1);
   assert(strcmp(_octal_mapper("001"), "1") == 0);
-  text_green("octal_mapper -> should return 1 for 001 value", 10);
+  text_green("_octal_mapper -> should return 1 for 001 value", 10);
   add_new_line(1);
   assert(strcmp(_octal_mapper("010"), "2") == 0);
-  text_green("octal_mapper -> should return 2 for 010 value", 10);
+  text_green("_octal_mapper -> should return 2 for 010 value", 10);
   add_new_line(1);
   assert(strcmp(_octal_mapper("011"), "3") == 0);
-  text_green("octal_mapper -> should return 3 for 011 value", 10);
+  text_green("_octal_mapper -> should return 3 for 011 value", 10);
   add_new_line(1);
   assert(strcmp(_octal_mapper("100"), "4") == 0);
-  text_green("octal_mapper -> should return 4 for 100 value", 10);
+  text_green("_octal_mapper -> should return 4 for 100 value", 10);
   add_new_line(1);
   assert(strcmp(_octal_mapper("101"), "5") == 0);
-  text_green("octal_mapper -> should return 5 for 101 value", 10);
+  text_green("_octal_mapper -> should return 5 for 101 value", 10);
   add_new_line(1);
   assert(strcmp(_octal_mapper("110"), "6") == 0);
-  text_green("octal_mapper -> should return 6 for 110 value", 10);
+  text_green("_octal_mapper -> should return 6 for 110 value", 10);
   add_new_line(1);
   assert(strcmp(_octal_mapper("111"), "7") == 0);
-  text_green("octal_mapper -> should return 7 for 111 value", 10);
+  text_green("_octal_mapper -> should return 7 for 111 value", 10);
 }
