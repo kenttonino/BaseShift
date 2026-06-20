3996f964d371e2d7005ad819a11ea0effc8ca71a
Kent Tonino <kenttonino.io@gmail.com>
2026-06-20 13:38:13 +0800

    chore: refactor to_decimal tests

--- src/menu/binary/__tests__/test_binary_to_decimal.c
+++ src/menu/binary/__tests__/test_binary_to_decimal.c
@@ -3,13 +3,13 @@
 #include "../../../utils/utils.h"
 
 void test_binary_to_decimal(void) {
-  text_white("[ Testing binary_to_decimal Functions ]", 10);
+  text_white("[ Testing to_decimal Functions ]", 10);
   add_new_line(1);
 
   assert(_get_decimal("1111") == 15);
-  text_green("get_decimal -> should return 15 for 1111", 10);
+  text_green("_get_decimal -> should return 15 for 1111 digits", 10);
   add_new_line(1);
 
   assert(_get_decimal_with_dot("1111.1") == 15.5);
-  text_green("get_decimal_with_dot -> should return 15.5 for 1111.1", 10);
+  text_green("_get_decimal_with_dot -> should return 15.5 for 1111.1 digits", 10);
 }
