8d4813021395b5e86e08aa3564b6f3315b557652
Kent Tonino <kenttonino.io@gmail.com>
2026-06-20 16:32:24 +0800

    chore: combine tests into one file

--- src/menu/helper/__tests__/helper_tests.c
+++ src/menu/helper/__tests__/helper_tests.c
@@ -1,21 +1,12 @@
 #include "../../../utils/utils.h"
-#include "./test_is_positive.c"
-#include "./test_is_positive_with_dot.c"
-#include "./test_is_negative.c"
-#include "./test_is_negative_with_dot.c"
+#include "./test_validate_digit.c"
 
 int main(void) {
   clear_terminal();
   add_new_line(2);
 
-  test_is_positive();
-  add_new_line(2);
-  test_is_positive_with_dot();
-  add_new_line(2);
-  test_is_negative();
-  add_new_line(2);
-  test_is_negative_with_dot();
+  test_validate_digit();
+  add_new_line(3);
 
-  add_new_line(2);
   return 0;
 }
