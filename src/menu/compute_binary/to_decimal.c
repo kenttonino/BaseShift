b981e710f1ff9c154cf3fe5dfa8f6f3da24505a6
Kent Tonino <kenttonino.io@gmail.com>
2026-06-21 15:45:49 +0800

    chore: cleanup

--- src/menu/compute_binary/to_decimal.c
+++ src/menu/compute_binary/to_decimal.c
@@ -5,77 +5,70 @@
 #include "../../utils/utils.h"
 #include "../helper/helper.h"
 
-// Compute the decimal value without dot.
-int _get_decimal(char *binary_input) {
-  char *reversed_binary_input = reverse_string(binary_input);
-  int reversed_binary_length = strlen(reversed_binary_input);
-  int decimal = 0;
+int _get_dec(char *bin_input) {
+  char *reversed_bin_input = reverse_string(bin_input);
+  int reversed_bin_length = strlen(reversed_bin_input);
+  int dec = 0;
 
-  for (int i = 0; i < reversed_binary_length; i++) {
+  for (int i = 0; i < reversed_bin_length; i++) {
     // Use the ASCII equivalent.
     // 0 -> 48
     // 1 -> 49
-    int current_binary_int = reversed_binary_input[i] - '0';
-    decimal = decimal + (current_binary_int * pow(2, i));
+    int current_bin_int = reversed_bin_input[i] - '0';
+    dec = dec + (current_bin_int * pow(2, i));
   }
 
-  return decimal;
+  return dec;
 }
 
-// Compute the decimal value with dot.
-double _get_decimal_with_dot(char *binary_input) {
-  // Separate the binary values before and after dot.
-  char *binary_before_dot = malloc(strlen(binary_input) + 1);
-  char *binary_after_dot = malloc(strlen(binary_input) + 1);
+double _get_dec_with_dot(char *bin_input) {
+  char *bin_before_dot = malloc(strlen(bin_input) + 1);
+  char *bin_after_dot = malloc(strlen(bin_input) + 1);
   int is_after_dot = 0;
   int counter_before_dot = 0;
   int counter_after_dot = 0;
-  for (int i = 0; binary_input[i] != '\0'; i++) {
-    if (binary_input[i] == '.') {
+  for (int i = 0; bin_input[i] != '\0'; i++) {
+    if (bin_input[i] == '.') {
       is_after_dot = 1;
       continue;
     }
 
     if (is_after_dot) {
-      binary_after_dot[counter_after_dot++] = binary_input[i];
+      bin_after_dot[counter_after_dot++] = bin_input[i];
       continue;
     } else {
-      binary_before_dot[counter_before_dot++] = binary_input[i];
+      bin_before_dot[counter_before_dot++] = bin_input[i];
     }
   }
 
-  // Reset to the null terminator to stop reading this variables.
-  binary_before_dot[counter_before_dot] = '\0';
-  binary_after_dot[counter_after_dot] = '\0';
+  bin_before_dot[counter_before_dot] = '\0';
+  bin_after_dot[counter_after_dot] = '\0';
 
-  // Compute the values before dot.
-  char *reversed_binary_input = reverse_string(binary_before_dot);
-  int reversed_binary_length = strlen(reversed_binary_input);
+  char *reversed_bin_input = reverse_string(bin_before_dot);
+  int reversed_bin_length = strlen(reversed_bin_input);
   double decimal = 0;
 
-  for (int i = 0; i < reversed_binary_length; i++) {
-    int current_binary_int = reversed_binary_input[i] - '0';
+  for (int i = 0; i < reversed_bin_length; i++) {
+    int current_binary_int = reversed_bin_input[i] - '0';
     decimal = decimal + (current_binary_int * pow(2, i));
   }
 
-  // Compute the values after dot.
-  for (int i = 0; i < (int) strlen(binary_after_dot); i++) {
+  for (int i = 0; i < (int) strlen(bin_after_dot); i++) {
     // Use the ASCII equivalent.
     // 0 -> 48
     // 1 -> 49
-    int current_binary_int = binary_after_dot[i] - '0';
+    int current_binary_int = bin_after_dot[i] - '0';
     decimal += current_binary_int * pow(2, -(i + 1));
   }
 
-  free(binary_before_dot);
-  free(binary_after_dot);
+  free(bin_before_dot);
+  free(bin_after_dot);
   return decimal;
 }
 
-// Output the binary to decimal result.
-void _display_decimal(char *decimal, int negative) {
-  char neg_decimal[1000] = "-";
-  strcat(neg_decimal, decimal);
+void _display_dec(char *dec, int negative) {
+  char neg_dec[1000] = "-";
+  strcat(neg_dec, dec);
 
   printf(
       "%-16s[%s %sDecimal%s %s]%s : %s%s%s",
@@ -86,7 +79,7 @@ void _display_decimal(char *decimal, int negative) {
       BLUE,
       RESET,
       YELLOW,
-      negative == 1 ? neg_decimal : decimal,
+      negative == 1 ? neg_dec : dec,
       RESET
   );
 }
@@ -97,10 +90,10 @@ void to_decimal(char *binary_input) {
     char *binary = malloc(sizeof(char) * 1000);
     strcpy(binary, binary_input);
 
-    int decimal = _get_decimal(binary);
+    int decimal = _get_dec(binary);
     char decimal_string[1000];
     sprintf(decimal_string, "%d", decimal);
-    _display_decimal(decimal_string, 0);
+    _display_dec(decimal_string, 0);
 
     free(binary);
     return;
@@ -114,10 +107,10 @@ void to_decimal(char *binary_input) {
     char *positive_binary = malloc(sizeof(char) * 1000);
     memmove(positive_binary, binary + 1, strlen(binary));
 
-    int decimal = _get_decimal(positive_binary);
+    int decimal = _get_dec(positive_binary);
     char decimal_string[1000];
     sprintf(decimal_string, "%d", decimal);
-    _display_decimal(decimal_string, 1);
+    _display_dec(decimal_string, 1);
 
     free(binary);
     free(positive_binary);
@@ -129,10 +122,10 @@ void to_decimal(char *binary_input) {
     char *binary = malloc(sizeof(char) * 1000);
     strcpy(binary, binary_input);
 
-    double decimal = _get_decimal_with_dot(binary);
+    double decimal = _get_dec_with_dot(binary);
     char decimal_string[1000];
     sprintf(decimal_string, "%.2f", decimal);
-    _display_decimal(decimal_string, 0);
+    _display_dec(decimal_string, 0);
 
     free(binary);
     return;
@@ -146,10 +139,10 @@ void to_decimal(char *binary_input) {
     char *positive_binary = malloc(sizeof(char) * 1000);
     memmove(positive_binary, binary + 1, strlen(binary));
 
-    double decimal = _get_decimal_with_dot(positive_binary);
+    double decimal = _get_dec_with_dot(positive_binary);
     char decimal_string[1000];
     sprintf(decimal_string, "%.2f", decimal);
-    _display_decimal(decimal_string, 1);
+    _display_dec(decimal_string, 1);
 
     free(binary);
     free(positive_binary);
