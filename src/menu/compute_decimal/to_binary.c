92235212e302f021a971aaeb5bd7615c89e7be5d
Kent Tonino <kenttonino.io@gmail.com>
2026-07-04 20:44:16 +0800

    feat: dotted decimal splitter

--- src/menu/compute_decimal/to_binary.c
+++ src/menu/compute_decimal/to_binary.c
@@ -3,6 +3,7 @@
 #include <string.h>
 #include "../helper/helper.h"
 #include "../../utils/utils.h"
+#include "./types.h"
 
 char *_get_bin(char *dec_input) {
   char *end_ptr;
@@ -30,10 +31,45 @@ char *_get_bin(char *dec_input) {
   return reverse_string(bin_digits);
 }
 
+DottedDecimal _get_sanitized_dotted_dec(char* dec_input) {
+  // Store the decimal input in new variable.
+  static char dec_buffer[1000];
+  memset(dec_buffer, 0, sizeof(char) * 1000);
+  memmove(dec_buffer, dec_input, strlen(dec_input));
 
-//char *_get_bin_with_dot(char *dec_input) {
-//    return "0.0001100110011001101";
-//}
+  // Sanitized the decimal values before and after dot.
+  static char dec_before_dot[1000];
+  static char dec_after_dot[1000];
+  memset(dec_before_dot, 0, sizeof(char) * 1000);
+  memset(dec_after_dot, 0, sizeof(char) * 1000);
+
+  int is_after_dot = 0;
+  int counter_before_dot = 0;
+  int counter_after_dot = 0;
+  for (int i = 0; dec_input[i] != '\0'; i++) {
+    if (dec_input[i] == '.') {
+      is_after_dot = 1;
+      continue;
+    }
+
+    if (is_after_dot) {
+      dec_after_dot[counter_after_dot++] = dec_input[i];
+      continue;
+    } else {
+      dec_before_dot[counter_before_dot++] = dec_input[i];
+    }
+  }
+
+  // Reset to the null terminator to stop reading this variables.
+  dec_before_dot[counter_before_dot] = '\0';
+  dec_after_dot[counter_after_dot] = '\0';
+
+  DottedDecimal sanitized_dec;
+  sanitized_dec.before_dot = dec_before_dot;
+  sanitized_dec.after_dot = dec_after_dot;
+
+  return sanitized_dec;
+}
 
 void _display_bin(char *bin, int negative) {
   char neg_bin[1000] = "-";
@@ -70,9 +106,22 @@ void to_binary(char *dec_input) {
   if (is_positive_with_dot(dec_input)) {
     char *dec = malloc(sizeof(char) * 1000);
     strcpy(dec, dec_input);
+    DottedDecimal dotted_dec = _get_sanitized_dotted_dec(dec);
+
+    char *dec_before_dot = malloc(sizeof(char) * 1000);
+    char *dec_after_dot = malloc(sizeof(char) * 1000);
+    strcpy(dec_before_dot, dotted_dec.before_dot);
+    strcpy(dec_after_dot, dotted_dec.after_dot);
+
+    printf("before_dot: %s", dec_before_dot);
+    add_new_line(1);
+    printf("after_dot: %s", dec_after_dot);
+    add_new_line(1);
 
     _display_bin("1111011.00011001", 0);
     free(dec);
+    free(dec_before_dot);
+    free(dec_after_dot);
     return;
   }
 
--- src/menu/compute_decimal/types.h
+++ src/menu/compute_decimal/types.h
@@ -1,9 +1,9 @@
-#ifndef COMPUTE_DECIMAL_H
-#define COMPUTE_DECIMAL_H
+#ifndef COMPUTE_DECIMAL_TYPES_H
+#define COMPUTE_DECIMAL_TYPES_H
 
 typedef struct {
-  char *before_dot;
-  char *after_dot;
+  char* before_dot;
+  char* after_dot;
 } DottedDecimal;
 
 #endif
