0fbb4884b5dffc557aee5c28c1ab9adb644f71b6
Kent Tonino <kenttonino.io@gmail.com>
2026-07-05 17:34:51 +0800

    feat: _get_sanitized_dotted_dec tests

--- src/menu/compute_decimal/__tests__/test_to_binary.c
+++ src/menu/compute_decimal/__tests__/test_to_binary.c
@@ -16,4 +16,14 @@ void test_to_binary(void) {
   memmove(dec_dot_input, "123", sizeof(char) * 4);
   assert(strcmp(_get_bin_with_dot(dec_dot_input), "00011111") == 0);
   text_green("_get_bin_with_dot -> should return 00011111 binary digits for 123 decimal dots digits", 10);
+  add_new_line(1);
+
+  char dotted_decimal_input[8];
+  memmove(dotted_decimal_input, "123.123", sizeof(char) * 8);
+  DottedDecimal dotted_decimal = _get_sanitized_dotted_dec(dotted_decimal_input);
+  assert(strcmp(dotted_decimal.before_dot, "123") == 0);
+  text_green("_get_sanitized_dotted_dec -> should return 123 decimal digits for before_dot 123.123 decimal digits", 10);
+  add_new_line(1);
+  assert(strcmp(dotted_decimal.after_dot, "123") == 0);
+  text_green("_get_sanitized_dotted_dec -> should return 123 decimal digits for after_dot 123.123 decimal digits.", 10);
 }
