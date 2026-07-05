0a2741d0ae0a90cbb175326bee81b416f5323cd1
Kent Tonino <kenttonino.io@gmail.com>
2026-07-05 17:25:00 +0800

    feat: _get_bin_with_dot tests

--- src/menu/compute_decimal/__tests__/test_to_binary.c
+++ src/menu/compute_decimal/__tests__/test_to_binary.c
@@ -10,4 +10,10 @@ void test_to_binary(void) {
   memmove(dec_input, "123", sizeof(char) * 4);
   assert(strcmp(_get_bin(dec_input), "1111011") == 0);
   text_green("_get_bin -> should return 1111011 binary digits for 123 decimal digits", 10);
+  add_new_line(1);
+
+  char dec_dot_input[5];
+  memmove(dec_dot_input, "123", sizeof(char) * 4);
+  assert(strcmp(_get_bin_with_dot(dec_dot_input), "00011111") == 0);
+  text_green("_get_bin_with_dot -> should return 00011111 binary digits for 123 decimal dots digits", 10);
 }
