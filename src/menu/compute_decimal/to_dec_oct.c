c6f80a1bcc98185e4273aec44b1688ec61af59c9
Kent Tonino <kenttonino.io@gmail.com>
2026-07-11 15:52:07 +0800

    feat: _get_dec_bin
    Implement a business logic to handle the computation of decimal to
    binary for positive digits without dot.
    
    Signed-off-by: Kent Tonino <kenttonino.io@gmail.com>

--- src/menu/compute_decimal/to_dec_oct.c
+++ src/menu/compute_decimal/to_dec_oct.c
@@ -1,7 +1,32 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include "../../utils/utils.h"
 #include "../helper/helper.h"
+
+char* _get_dec_oct(char* dec_input) {
+  char* end_ptr;
+  int int_dec_input = strtol(dec_input, &end_ptr, 10);
+
+  static char oct_digits[1000];
+  memset(oct_digits, 0, sizeof(char) * 1000);
+  int dividend = int_dec_input;
+
+  for (;;) {
+    if (dividend != 0) {
+      int reminder = dividend % 8;
+      char char_reminder[1000];
+      sprintf(char_reminder, "%d", reminder);
+      strcat(oct_digits, char_reminder);
+      dividend = dividend / 8;
+      continue;
+    }
+
+    break;
+  }
+
+  return reverse_string(oct_digits);
+}
 
 void _display_dec_oct(char *oct, int negative) {
   char neg_oct[1000] = "-";
@@ -25,9 +50,11 @@ void to_dec_oct(char* dec_input) {
   if (is_positive(dec_input)) {
     char* dec = malloc(sizeof(char) * 1000);
     strcpy(dec, dec_input);
-    printf("dec: %s", dec_input);
-    add_new_line(1);
-    _display_dec_oct("173", 0);
+
+    char* oct = _get_dec_oct(dec);
+    _display_dec_oct(oct, 0);
+
+    free(dec);
     return;
   }
 }
