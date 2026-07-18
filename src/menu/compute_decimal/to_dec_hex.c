f64daa6a47f9ccc0bad7898841cd5f4a018540d1
Kent Tonino <kenttonino.io@gmail.com>
2026-07-18 23:55:50 +0800

    feat: dec to hex computation

--- src/menu/compute_decimal/to_dec_hex.c
+++ src/menu/compute_decimal/to_dec_hex.c
@@ -1,14 +1,42 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
 #include "../../utils/utils.h"
 #include "../helper/helper.h"
-#include <stdlib.h>
+
+char * _dec_hex_mapper(char* dec) {
+  if (strcmp(dec, "10") == 0) return "A";
+  if (strcmp(dec, "11") == 0) return "B";
+  if (strcmp(dec, "12") == 0) return "C";
+  if (strcmp(dec, "13") == 0) return "D";
+  if (strcmp(dec, "14") == 0) return "E";
+  if (strcmp(dec, "15") == 0) return "F";
+  return dec;
+}
 
 char* _get_dec_hex(char* dec) {
   char* dec_int_end_ptr;
   int int_dec = strtol(dec, &dec_int_end_ptr, 10);
-  printf("%d", int_dec);
-  add_new_line(1);
 
-  return "7B";
+  static char hex[1000];
+  memset(hex, 0, sizeof(char) * 1000);
+  int dividend = int_dec;
+  for (;;) {
+    if (dividend != 0) {
+      int rem = dividend % 16;
+      printf("rem: %d", rem);
+      add_new_line(1);
+      char char_rem[1000];
+      sprintf(char_rem, "%d", rem);
+      strcat(hex, _dec_hex_mapper(char_rem));
+      dividend = dividend / 16;
+      continue;
+    }
+
+    break;
+  }
+
+  return reverse_string(hex);
 }
 
 void _display_dec_hex(char *hex, int negative) {
