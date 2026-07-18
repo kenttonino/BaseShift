429b2dba30bf3b4e23e6d9a32f1e2a9ec0faaf5b
Kent Tonino <kenttonino.io@gmail.com>
2026-07-18 13:17:38 +0800

    feat: setup _get_dec_hex function

--- src/menu/compute_decimal/to_dec_hex.c
+++ src/menu/compute_decimal/to_dec_hex.c
@@ -1,6 +1,13 @@
 #include "../../utils/utils.h"
 #include "../helper/helper.h"
 
+char* _get_dec_hex(char* dec) {
+  printf("%s", dec);
+  add_new_line(1);
+
+  return "7B";
+}
+
 void _display_dec_hex(char *hex, int negative) {
   char neg_hex[1000] = "-";
   strcat(neg_hex, hex);
@@ -24,9 +31,8 @@ void to_dec_hex(char* dec_input) {
     char* dec = malloc(sizeof(char) * 1000);
     strcpy(dec, dec_input);
 
-    printf("%s", dec);
-    add_new_line(1);
-    _display_dec_hex("7B", 0);
+    char* hex = _get_dec_hex(dec);
+    _display_dec_hex(hex, 0);
 
     free(dec);
     return;
