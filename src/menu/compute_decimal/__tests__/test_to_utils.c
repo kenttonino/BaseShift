45b413a0a27e41021afc0b7e5c8a5ca85dca8afc
Kent Tonino <kenttonino.io@gmail.com>
2026-08-22 18:17:24 +0800

    fix: compute_decimal_tests

--- src/menu/compute_decimal/__tests__/test_to_utils.c
+++ src/menu/compute_decimal/__tests__/test_to_utils.c
@@ -1,6 +1,6 @@
 #include <assert.h>
 #include "../../../utils/utils.h"
-#include "../to_utils.h"
+#include "../../helper/helper.h"
 
 void test_to_utils(void) {
   text_white("[ Testing to_dec_utils Functions ]", 10);
@@ -8,16 +8,16 @@ void test_to_utils(void) {
 
   char radixp_adder_input[5];
   memmove(radixp_adder_input, "123", sizeof(char) * 4);
-  assert(strcmp(_dec_radixp_adder(radixp_adder_input), "0.123") == 0);
-  text_green("_dec_radixp_adder -> should return 0.123 for 123 inputs", 10);
+  assert(strcmp(radixp_adder(radixp_adder_input), "0.123") == 0);
+  text_green("radixp_adder -> should return 0.123 for 123 inputs", 10);
   add_new_line(1);
 
   char radixp_dec_input[8];
   memmove(radixp_dec_input, "123.123", sizeof(char) * 8);
-  GenericInput generic_input = _get_dec_generic_input(radixp_dec_input);
+  GenericInput generic_input = get_generic_input(radixp_dec_input);
   assert(strcmp(generic_input.before_radixp, "123") == 0);
-  text_green("_get_dec_generic_input -> should return 123 before_dot for 123.123 inputs", 10);
+  text_green("get_generic_input -> should return 123 before_dot for 123.123 inputs", 10);
   add_new_line(1);
   assert(strcmp(generic_input.after_radixp, "123") == 0);
-  text_green("_get_dec_generic_input -> should return 123 after_dot for 123.123 inputs", 10);
+  text_green("get_generic_input -> should return 123 after_dot for 123.123 inputs", 10);
 }
