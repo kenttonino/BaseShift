aeb5d05c71fc357ae2b0eb83efc1961d9013a46d
Kent Tonino <kenttonino.io@gmail.com>
2026-09-20 23:11:25 +0800

    fix: pointer consistency

--- src/menu/helper/helper.h
+++ src/menu/helper/helper.h
@@ -8,11 +8,11 @@
 extern char* radixp_adder(char* input);
 extern GenericInput get_generic_input(char* input);
 extern char* get_bin_hex_mapper(char* bin);
-extern char* reverse_string(char *input);
-extern int is_negative(char *input);
-extern int is_negative_radixp(char *input);
-extern int is_positive(char *input);
-extern int is_positive_radixp(char *input);
-extern ZeroPrefix is_zero_only(char *input);
+extern char* reverse_string(char* input);
+extern int is_negative(char* input);
+extern int is_negative_radixp(char* input);
+extern int is_positive(char* input);
+extern int is_positive_radixp(char* input);
+extern ZeroPrefix is_zero_only(char* input);
 
 #endif
